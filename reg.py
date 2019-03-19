e1ba0ade4e74d58129b5681d16b2664b756967f7
Wang Zhiyong <wangzhiyong_vendor@domain.sensetime.com>
2019-03-19 15:55:23 +0800

    add content

--- reg.py
+++ reg.py
@@ -2,3 +2,11 @@ str1="hello,python"
 print(str1)
 list1=['a','b','c']
 print(list1)
+print("hello,world")
+print("hello,world")
+print("hello,world")
+print("hello,world")
+print("hello,world")
+print("hello,world")
+print("hello,world")
+
